a90d68eebbdbe3ec0a995512697a9c6f479f8a2a
Dave Giunta <dgiunta@mac.com>
2012-06-03 02:55:58 -0500

    daveg: changing settings back to creating actual settings instead of stupid defaults.

--- config/initializers/settings.rb
+++ config/initializers/settings.rb
@@ -1,7 +1,7 @@
-Setting.defaults['resume_url'] ||= 'http://google.com'
-Setting.defaults['contact.title'] ||= 'Hello!'
+Setting['resume_url'] ||= 'http://google.com'
+Setting['contact.title'] ||= 'Hello!'
 
-Setting.defaults['contact.intro'] ||= <<-END_INTRO
+Setting['contact.intro'] ||= <<-END_INTRO
   My name is Alyssa and I am a recent graduate of the Illinois Institute of Art- Schaumburg. I am a graphic designer and illustrator with experience in print production, collateral, and multi-media design.
 
   I frequently incorporate elements of motion and interactivity in my designs providing my viewers with a fun and engaging experience. While I am most interested in designing for a deserving cause and social reform, no job is too big or too small.
@@ -9,5 +9,5 @@ Setting.defaults['contact.intro'] ||= <<-END_INTRO
   With that... let's make great design!
 END_INTRO
 
-Setting.defaults['contact.phone'] ||= '708.927.3933'
-Setting.defaults['contact.email'] ||= 'montalto.alyssa@gmail.com'
+Setting['contact.phone'] ||= '708.927.3933'
+Setting['contact.email'] ||= 'montalto.alyssa@gmail.com'
